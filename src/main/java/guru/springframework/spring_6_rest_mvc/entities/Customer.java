2c81a0a02b55f821f23df01c3f9c237654b02edc
vuthaphai <vuthaphai@gmail.com>
2024-09-21 19:45:11 +0700

    jpa update for mysql

--- src/main/java/guru/springframework/spring_6_rest_mvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring_6_rest_mvc/entities/Customer.java
@@ -3,6 +3,8 @@ package guru.springframework.spring_6_rest_mvc.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,6 +19,7 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String name;
