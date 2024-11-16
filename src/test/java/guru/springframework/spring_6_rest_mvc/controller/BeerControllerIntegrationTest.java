b1fa2e7b452daa196ec2415f6b830d614db0ff90
Vutha Phai <vuthaphai@Vuthas-MacBook-Pro.local>
2024-11-16 19:12:39 +0700

    wip Spring security

--- src/test/java/guru/springframework/spring_6_rest_mvc/controller/BeerControllerIntegrationTest.java
+++ src/test/java/guru/springframework/spring_6_rest_mvc/controller/BeerControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
@@ -24,6 +25,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -36,11 +38,13 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-//@Import(SpringSecurityConfig.class)
+@AutoConfigureMockMvc
+@Import(SpringSecurityConfig.class)
 @ActiveProfiles("localmysql") // Activate profile to use BeerServiceIJPA
 class BeerControllerIntegrationTest {
     
@@ -68,6 +72,17 @@ class BeerControllerIntegrationTest {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .apply(springSecurity()) // Apply Spring Security to MockMvc
                 .build();
+    }
+
+    @Test void testAuthenticatedRequest() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH)
+                        .with(httpBasic(BeerControllerTest.USERNAME,BeerControllerTest.PASSWORD)))
+                .andExpect(status().isOk());
+    }
+
+    @Test void testUnauthorizedRequest() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
@@ -204,6 +219,36 @@ class BeerControllerIntegrationTest {
         Beer updateBeer = beerRepository.findById(beer.getId()).get();
         assertThat(updateBeer.getBeerName()).isEqualTo(beerName);
 
+    }
+
+    @Rollback
+    @Transactional
+    @Test void testSaveNewBeerPOST() throws Exception {
+        BeerDTO beerDTO = BeerDTO.builder()
+                .beerName("New beer")
+                .beerStyle(BeerStyle.IPA)
+                .upc("1234567")
+                .quantityOnHand(1234)
+                .price(new BigDecimal("11.11"))
+                .build();
+        MvcResult result = mockMvc.perform(post(BeerController.BEER_PATH)
+                .with(httpBasic(BeerControllerTest.USERNAME,BeerControllerTest.PASSWORD)) // Add authentication
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(status().isCreated())
+                .andReturn();
+        String[] locationUUID = result.getResponse().getHeader("Location").split("/");
+        UUID savedUUID = UUID.fromString(locationUUID[4]);
+        Beer beer = beerRepository.findById(savedUUID).orElse(null);
+        assertThat(beer).isNotNull();
+    }
+
+    @Test void testUnauthorizedSaveNewBeer() throws Exception {
+        BeerDTO beerDTO = BeerDTO.builder() .beerName("New beer")
+                .build(); mockMvc.perform(post(BeerController.BEER_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(status().isUnauthorized());
     }
 
     @Rollback
